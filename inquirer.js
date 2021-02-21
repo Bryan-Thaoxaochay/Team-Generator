a49409db3e58503e282449bd4edd4e3e9c07dd72
LittleMagician <bthaoxaochay@gmail.com>
2021-02-21 12:50:04 -0600

    created class.js

--- inquirer.js
+++ inquirer.js
@@ -1,2 +1,17 @@
 const inquirer = require('inquirer');
 
+inquirer
+    .prompt([
+        {
+
+        },
+    ]) // Inquirer
+    .then((responses) => {
+
+        const README = generateREADME(responses);
+
+        fs.writeFile("README.md", README, (err) =>
+            err ? console.error(err) : console.log("MD Created")
+        );
+
+    }) // Then
