075f7c8e9e213c41ffd6ea9bca584bd1621a42f8
LittleMagician <bthaoxaochay@gmail.com>
2021-02-21 13:09:30 -0600

    added name, role, and email questions to inquirer prompt

--- inquirer.js
+++ inquirer.js
@@ -3,15 +3,26 @@ const inquirer = require('inquirer');
 inquirer
     .prompt([
         {
-
+            type: 'input',
+            name: 'name',
+            message: 'What is the team members full name? (i.e. Bryan Thaoxaochay)'
+        },
+        {
+            type: 'checkbox',
+            name: 'role',
+            message: 'What is their role?',
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is their email?'
         },
     ]) // Inquirer
     .then((responses) => {
 
-        const README = generateREADME(responses);
-
-        fs.writeFile("README.md", README, (err) =>
-            err ? console.error(err) : console.log("MD Created")
-        );
+        // fs.writeFile("README.md", README, (err) =>
+        //     err ? console.error(err) : console.log("MD Created")
+        // );
 
     }) // Then
